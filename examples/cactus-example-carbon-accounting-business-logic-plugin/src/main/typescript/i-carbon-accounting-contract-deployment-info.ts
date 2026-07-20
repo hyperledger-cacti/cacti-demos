8089ba02d93290202f657fb318e4ecaf67fedf03
ParthSingh <posiedon.1721@gmail.com>
2026-07-20 19:34:29 +0530

    feat: migrate carbon-accounting packages (#22)
    Signed-off-by: Parth Singh <posiedon.1721@gmail.com>

---
+++ examples/cactus-example-carbon-accounting-business-logic-plugin/src/main/typescript/i-carbon-accounting-contract-deployment-info.ts
@@ -0,0 +1,5 @@
+import { IFabricContractDeployment } from "./i-fabric-contract-deployment";
+
+export interface ICarbonAccountingFabricContractDeploymentInfo {
+  emissions: IFabricContractDeployment;
+}
